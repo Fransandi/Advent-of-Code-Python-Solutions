64de067a72bf46f1a7dac53489168e24cbd40074
Francisco Sandi <fransandi@gmail.com>
2022-12-15 20:11:29 +0100

    Day 15: Beacon Exclusion Zone

--- 2022/solutions/day15.py
+++ 2022/solutions/day15.py
@@ -7,33 +7,46 @@
 =========================================
 '''
 
-EXAMPLE_Y = 10
-EXERCISE_Y = 2000000
 
 ### Part One
 def part_one(input):
-    sensors, distance = parse_input(input)
-    min_x = sorted([sensor[0]-distance[sensor] for sensor in sensors.keys()])[0]
-    max_x = sorted([sensor[0]+distance[sensor]+1 for sensor in sensors.keys()])[-1]
+    sensors = parse_input(input)
 
-    # Condition to switch between the 'y' value in the example and exercise
-    y = EXAMPLE_Y if max_x < EXERCISE_Y else EXERCISE_Y
+    # Condition to switch between the 'row' value in the example and exercise
+    row = 10 if all(abs(key[0])<1000 for key in sensors.keys()) else 2000000
+
+    # Get all sensor's area segments touching the row to check
+    segments = []
+    for (s_x, s_y), dist in sensors.items():
+        # Skip if area doesn't touch the row
+        if s_y >= row and s_y-dist>row: continue
+        if s_y < row and s_y+dist<row: continue
+
+        segments.append((s_x - (dist-abs(s_y-row)), s_x + (dist-abs(s_y-row))))
     
-    # Evaluate all the points in the horizontal line
-    count = 0
-    for point in [(x, y) for x in range(min_x, max_x+1) if (x, y) not in sensors.values()]:
-        # Count 1 if any sensor invalidates this point to have a beacon
-        count += point_reachable_by_sensors(distance, point)
+    # Merge all the overlapping segments
+    segments.sort()
+    merged_segments = [segments.pop(0)]
+    while segments:
+        prev = merged_segments[-1]
+        next = segments.pop(0)
 
-    return count
+        if prev[1]<next[0]: 
+            merged_segments.append(next)
+            continue
+
+        merged_segments[-1] = ((min(prev[0], next[0]), max(prev[1], next[1])))
+
+    # Return the sum of lenghts from all the segments
+    return sum([segment[1]-segment[0] for segment in merged_segments])
 
 ### Part Two
 def part_two(input):
-    sensors, distance = parse_input(input)
+    sensors = parse_input(input)
 
     # Calculate edges of all sensor areas in both directions
-    lines_dir1 = [s[0]+s[1]+directions for s, d in distance.items() for directions in (d, -d)]
-    lines_dir2 = [s[0]-s[1]+directions for s, d in distance.items() for directions in (d, -d)]
+    lines_dir1 = [s[0]+s[1]+directions for s, d in sensors.items() for directions in (d, -d)]
+    lines_dir2 = [s[0]-s[1]+directions for s, d in sensors.items() for directions in (d, -d)]
 
     # Determine the line candidates based on distance between edges
     candidates_dir1, candidates_dir2 = [], []
@@ -47,18 +60,17 @@ def part_two(input):
 
     # Check all interesections between line candidates, until discovering the point not reachable by the sensors
     for (x, y) in all_intersections(candidates_dir1, candidates_dir2):
-        if not point_reachable_by_sensors(distance, (x, y)): return x * 4000000 + y
+        if not point_reachable_by_sensors(sensors, (x, y)): return x * 4000000 + y
 
 
 def parse_input(input):
-    sensors, distance = {}, {}
+    sensors = {}
     for line in input:
         instruction = line.split()
         sensor = (int(instruction[2].split('=')[-1][:-1]), int(instruction[3].split('=')[-1][:-1]))
         beacon = (int(instruction[8].split('=')[-1][:-1]), int(instruction[9].split('=')[-1]))
-        sensors[sensor] = beacon
-        distance[sensor] = manhattan_distance(sensor, beacon)
-    return sensors, distance
+        sensors[sensor] = manhattan_distance(sensor, beacon)
+    return sensors
 
 def manhattan_distance(a, b): 
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
@@ -66,5 +78,5 @@ def manhattan_distance(a, b):
 def all_intersections(points_1, points_2): 
     return [((p1+p2)//2, (p2-p1)//2) for p1 in list(set(points_1)) for p2 in list(set(points_2))]
 
-def point_reachable_by_sensors(distance, point):
-    return any([manhattan_distance(sensor, point) <= dist for sensor, dist in distance.items()])
+def point_reachable_by_sensors(sensors, point):
+    return any([manhattan_distance(sensor, point) <= distance for sensor, distance in sensors.items()])
