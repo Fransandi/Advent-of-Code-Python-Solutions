95522f9920c1a92f7637b07a6812148a1614e520
Francisco Sandi <fransandi@gmail.com>
2022-12-17 22:15:10 +0100

    Day 17: Pyroclastic Flow

--- 2022/solutions/day17.py
+++ 2022/solutions/day17.py
@@ -12,103 +12,11 @@ LEFT = '<'
 
 ### Part One
 def part_one(input):
-    return simulate(input, 2022)
-
+    return simulate(input[0], 2022)
 
 ### Part Two
 def part_two(input):
-
-    # instruction = input[0]
-
-    # a, b = 35, 53
-
-    # num = 1000000000000
-    num = 2022
-
-    return simulate(input, 2022)
-
-    return 0
-
-
-    # return simulate(instruction, num%a) + (num//a)*b
-
-
-    moves, round = input[0], 0
-    rock, next_rock = set(), 0
-    floor = set([(x, 0) for x in range(7)])
-    stopped_rocks, max_height = 0, 0
-
-
-    memo = set()
-    states = {}
-
-
-    # a = 1000000000000//35
-    # print('a', a)
-
-    # return 0
-
-    89, 142, 195
-
-    while True:
-        move = moves[round]
-
-        # Finish condition
-        # if stopped_rocks == 28571428571:
-        if stopped_rocks == 27: 
-            
-            # for y in range(max_height):
-            #     if all((x, y) in floor for x in range(7)): print(y)
-            
-            return max_height
-        
-        if not len(rock):
-            rock = get_next_rock(next_rock, max_height)
-            next_rock = (next_rock+1)%5
-
-            # for y in range(max_height, max_height+5):
-            #     if all((x, y) in floor for x in range(7)): print(y)
-
-        # Move either to the left or right if possible
-        if move == RIGHT: rock = move_to_right(rock, floor)
-        elif move == LEFT: rock = move_to_left(rock, floor)
-
-        # Move the rock down if possible
-        if can_go_down(rock, floor): rock = move_down(rock)
-        else:
-            # Rock has stopped and becomes part of the floor
-            max_height = max(max_height, max([y for (_, y) in rock]))
-
-            # if any(max_height == y for (_, y) in rock): print(rock)
-
-            floor.update(rock)
-            rock.clear()
-            stopped_rocks+=1
-            
-            hash = str(next_rock) + '-' + str(round) + '-'
-            for y in range(10): hash += ''.join(['#' if (x, max_height-y) in floor else '.' for x in range(7)])+ '|'
-
-            # print('HASH: ', hash)
-            if hash in memo:
-                print('REPETIDO', hash, states[hash], max_height)
-
-                # return -1
-            memo.add(hash)
-
-            if hash not in states: 
-                states[hash] = [stopped_rocks]
-            else: states[hash].append(stopped_rocks)
-
-
-            
-
-            # if all((x, max_height) in floor for x in range(7)):                
-            #     print(round, '-' ,stopped_rocks)
-
-            
-        
-        round = (round+1)%len(moves)
-
+    return simulate(input[0], 1000000000000)
 
 def get_next_rock(next_rock, max_height):
     x, y = 2, max_height+4
@@ -133,14 +41,15 @@ def can_go_down(rock, floor): return not any((x, y-1) in floor for (x, y) in lis
 
 def move_down(rock): return set([(x, y-1) for (x, y) in list(rock)])
 
-def simulate(input, num):
-    moves, round = input[0], 0
+def simulate(moves, num):
     rock, next_rock = set(), 0
     floor = set([(x, 0) for x in range(7)])
-    stopped_rocks, max_height = 0, 0
+    stopped_rocks, max_height, round = 0, 0, 0
+    memo = set()
+    states = {}
 
     while True:
-        move = moves[round%len(moves)]
+        move = moves[round]
 
         # Finish condition
         if stopped_rocks == num: return max_height
@@ -161,54 +70,22 @@ def simulate(input, num):
             floor.update(rock)
             rock.clear()
             stopped_rocks+=1
-        
-        round+=1
 
-
-def get_cycle_values(input):
-    moves, round = input[0], 0
-    rock, next_rock = set(), 0
-    floor = set([(x, 0) for x in range(7)])
-    stopped_rocks, max_height = 0, 0
-
-    memo = set()
-    states = {}
-
-    while True:
-        move = moves[round%len(moves)]
-
-        # Finish condition
-        if stopped_rocks == 10000: return 1, 2
-        
-        if not len(rock):
-            rock = get_next_rock(next_rock, max_height)
-            next_rock = (next_rock+1)%5
-
-        # Move either to the left or right if possible
-        if move == RIGHT: rock = move_to_right(rock, floor)
-        elif move == LEFT: rock = move_to_left(rock, floor)
-
-        # Move the rock down if possible
-        if can_go_down(rock, floor): rock = move_down(rock)
-        else:
-            # Rock has stopped and becomes part of the floor
-            max_height = max(max_height, max([y for (_, y) in rock]))
-            floor.update(rock)
-            rock.clear()
-            stopped_rocks+=1
-
-            hash = str(next_rock) + '-' + str(round) + '-'
-            for y in range(5): hash += ''.join(['#' if (x, max_height-y) in floor else '.' for x in range(7)])+ '|'
-
-            # print('HASH: ', hash)
-            if hash in memo:
-                print('REPETIDO', hash, states[hash], max_height)
-
-                # return -1
+            # Hash state to compare later and find the cycle
+            hash = hash_state(next_rock, round, max_height, floor)
             memo.add(hash)
+            if hash not in states: states[hash] = [(stopped_rocks, max_height)]
+            else: states[hash].append((stopped_rocks, max_height))
 
-            if hash not in states: 
-                states[hash] = [stopped_rocks]
-            else: states[hash].append(stopped_rocks)
-        
-        round+=1
+            # Cycle was found, calculate based on a simulation with a smaller value
+            if hash in memo and len(states[hash])>1:
+                rocks_cycle, height_cycle = states[hash][1][0] - states[hash][0][0], states[hash][1][1] - states[hash][0][1]
+                return simulate(moves, num%rocks_cycle) + (num//rocks_cycle)*height_cycle
+
+        round = (round+1)%len(moves)
+
+
+def hash_state(next_rock, round, max_height, floor):
+    hash = str(next_rock) + '-' + str(round) + '-'
+    for y in range(20): hash += ''.join(['#' if (x, max_height-y) in floor else '.' for x in range(7)])+ '|'
+    return hash
