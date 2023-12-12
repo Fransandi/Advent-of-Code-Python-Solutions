8d73ab92badb03c0c6345c7cf41b32eaa2dfecf7
Fransandi <fransandi@gmail.com>
2023-12-12 00:27:13 -0400

    Day 8: Haunted Wasteland

--- 2023/example/outputs.py
+++ 2023/example/outputs.py
@@ -28,8 +28,8 @@ example_outputs = {
         'part_two': 5905,
     },
     'day8': {
-        'part_one': None,
-        'part_two': None,
+        'part_one': 2,
+        'part_two': 6,
     },
     'day9': {
         'part_one': None,
--- 2023/solutions/day8.py
+++ 2023/solutions/day8.py
@@ -3,16 +3,63 @@
 || 🎄 Advent of Code 2023: Day 8 🗓
 || Link: https://adventofcode.com/2023
 || Template by: @fransandi
-|| Solution by: ...
+|| Solution by: @fransandi
 =========================================
 '''
 
+import copy
+import math
 
-### Part One
+
+# Part One
 def part_one(input):
-    pass
+    instructions, map = parse_input(input)
+    steps, instruction = 0, 0
 
-### Part Two
+    node = "AAA"
+    while True:
+        node = map[node][instructions[instruction]]
+        instruction = (instruction + 1) % len(instructions)
+        steps += 1
+
+        # Found
+        if node == "ZZZ":
+            return steps
+
+
+# Part Two
 def part_two(input):
-    pass
-    
+    instructions, map = parse_input(input)
+    steps, instruction = 0, 0
+    start = [node for node in map.keys() if node.endswith("A")]
+    nodes = copy.copy(start)
+
+    positions = {i: [] for i in range(len(nodes))}
+    while any([len(nodes) < 2 for nodes in positions.values()]):
+        for i in range(len(nodes)):
+            nodes[i] = map[nodes[i]][instructions[instruction]]
+
+            if nodes[i].endswith("Z") and len(positions[i]) < 2:
+                positions[i].append(steps)
+
+        instruction = (instruction + 1) % len(instructions)
+        steps += 1
+
+    return math.lcm(*[r[1] - r[0] for r in positions.values()])
+
+
+def parse_input(input):
+    instructions, map = "", {}
+    for line in [line.strip() for line in input]:
+        if line.strip():
+            if not instructions:
+                instructions = line
+            else:
+                node, children = [value.strip() for value in line.split("=")]
+                children = children[1:-1].split(", ")
+                map[node] = {
+                    'L': children[0],
+                    'R': children[1]
+                }
+
+    return instructions, map
